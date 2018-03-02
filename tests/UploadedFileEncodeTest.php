195cfbd088dec18e78f38bbee025a46ed63f3e19
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-03-02 22:43:38 +0100

    (JSON) uploaded files tests

---
+++ tests/UploadedFileEncodeTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace WyriHaximus\Tests;
+
+use PHPUnit\Framework\TestCase;
+use React\Http\Io\UploadedFile;
+use WyriHaximus;
+use function RingCentral\Psr7\stream_for;
+
+final class UploadedFileEncodeTest extends TestCase
+{
+    public function test()
+    {
+        $beerBottle = new UploadedFile(stream_for('Dark Horizon 5'), 14, UPLOAD_ERR_OK, 'beer.bottle', 'earth/liquid');
+
+        $json = WyriHaximus\psr7_uploaded_file_encode($beerBottle);
+        self::assertSame(
+            [
+                'filename' => 'beer.bottle',
+                'media_type' => 'earth/liquid',
+                'error' => 0,
+                'size' => 14,
+                'stream' => 'Dark Horizon 5',
+            ],
+            $json
+        );
+    }
+}
